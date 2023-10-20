7e5074f01067c281687e517ae650c7a7e39cf6db
jtrull101 <33765387+jtrull101@users.noreply.github.com>
2023-10-20 15:36:15 -0400

    initial commit

---
+++ angularclient/src/app/image-prediction.spec.ts
@@ -0,0 +1,7 @@
+import { ImagePrediction } from './image-prediction';
+
+describe('ImagePrediction', () => {
+  it('should create an instance', () => {
+    expect(new ImagePrediction()).toBeTruthy();
+  });
+});
